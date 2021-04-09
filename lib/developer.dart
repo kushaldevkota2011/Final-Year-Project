2f974bb348fd6c73eb98a9e9b0ede7b7bf60d0a2
kushaldevkota2011 <kushaldevkota2011@gmail.com>
2021-04-09 22:02:13 +0545

    commit

--- lib/developer.dart
+++ lib/developer.dart
@@ -1,17 +1,81 @@
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+// import './developer.dart';
+import 'package:http/http.dart' as http;
+
+
+class Notices {
+  final int id;
+  final String title;
+  final String description;
+  
+
+  Notices({this.id, this.title, this.description});
+
+  factory Notices.fromJson(Map<String, dynamic> json) {
+    return Notices(
+      id: json['id'],
+      title: json['title'],
+      description: json['description'],
+      // created_at: json['created_at'],
+    );
+  }
+}
 
 class DeveloperPage extends StatefulWidget {
+
+   final int value;
+
+  DeveloperPage({Key key,this.value}):super(key:key);
+
   @override
   _DeveloperPageState createState() => _DeveloperPageState();
 }
 
 class _DeveloperPageState extends State<DeveloperPage> {
-  @override
+
+ @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Developer's Profile"),
+        title: Text("Notice Description"),
+       
+      ),
+      //  body:Text('${widget.value}'),
+      body: FutureBuilder<List<Notices>>(
+        future: _fetchNoticess(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            List<Notices> data = snapshot.data;
+            return Text('${data[widget.value].description}');
+            
+          } else if (snapshot.hasError) {
+            return Text("${snapshot.error}");
+          }
+           return CircularProgressIndicator();
+        },
       ),
     );
   }
 }
+
+Future<List<Notices>> _fetchNoticess() async {
+    final response =
+        await http.get(Uri.parse('http://127.0.0.1:8000/api/notice/'));
+
+    //print(response.statusCode);
+
+    if (response.statusCode == 200) {
+      
+      List jsonResponse = json.decode(response.body);
+      // print(jsonResponse);
+      return jsonResponse
+          .map((notices) => new Notices.fromJson(notices))
+          .toList();
+      
+    } else {
+      throw Exception('Failed to load jobs from API');
+    }
+  }
--- lib/notice.dart
+++ lib/notice.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-
+import './developer.dart';
 import 'package:http/http.dart' as http;
 
 void main() => runApp(MaterialApp(
@@ -13,18 +13,25 @@ void main() => runApp(MaterialApp(
 class Notices {
   final int id;
   final String title;
+  final String created_at;
 
-  Notices({this.id, this.title});
+  Notices({this.id, this.title, this.created_at});
 
   factory Notices.fromJson(Map<String, dynamic> json) {
     return Notices(
       id: json['id'],
       title: json['title'],
+      created_at: json['created_at'],
     );
   }
 }
 
-class NoticePage extends StatelessWidget {
+class NoticePage extends StatefulWidget {
+  @override
+  _NoticePageState createState() => _NoticePageState();
+}
+
+class _NoticePageState extends State<NoticePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,7 +43,8 @@ class NoticePage extends StatelessWidget {
         builder: (context, snapshot) {
           if (snapshot.hasData) {
             List<Notices> data = snapshot.data;
-            return _NoticePage(data);
+            return _NoticePage(data, context);
+            
           } else if (snapshot.hasError) {
             return Text("${snapshot.error}");
           }
@@ -52,30 +60,70 @@ class NoticePage extends StatelessWidget {
 
     if (response.statusCode == 200) {
       List jsonResponse = json.decode(response.body);
-      return jsonResponse.map((notices) => new Notices.fromJson(notices)).toList();
+      return jsonResponse
+          .map((notices) => new Notices.fromJson(notices))
+          .toList();
     } else {
       throw Exception('Failed to load jobs from API');
     }
   }
 
-  // ignore: non_constant_identifier_names
-  ListView _NoticePage(data) {
+  ListView _NoticePage(data, BuildContext context) {
     return ListView.builder(
         itemCount: data.length,
         itemBuilder: (context, index) {
-          return _tile(data[index].title, Icons.announcement_outlined);
+          // return _tile(data[index].title, Icons.announcement_outlined);
+          // return Text('${data[index].title}');
+          // var d = data;
+          return getCard(index, data, context);
         });
   }
 
-  ListTile _tile(String title, IconData icon) => ListTile(
-        title: Text(title,
-            style: TextStyle(
-              fontWeight: FontWeight.w500,
-              fontSize: 20,
-            )),
-        leading: Icon(
-          icon,
-          color: Colors.blue[500],
+  Widget getCard(int index, List data, BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        var   route = MaterialPageRoute(builder:(BuildContext context)=> new DeveloperPage(value: index) );
+        Navigator.of(context).push(route);
+        // Navigator.push(
+        //     context,
+        //     new MaterialPageRoute(
+        //         builder: (BuildContext context) => new DeveloperPage(value: index),
+        //         // settings: RouteSettings(
+        //         //   arguments: index,
+        //         //),
+        //         ),);
+      },
+      child: Container(
+        height: 130,
+        child: Card(
+          elevation: 5,
+          margin: EdgeInsets.all(10),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              //  Container(child:Text('${data[0].created_at}')),
+
+              SizedBox(width: 20),
+
+              Image.asset(
+                "img/logo.png",
+                width: 80,
+                height: 80,
+              ),
+
+              SizedBox(width: 20),
+
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text('${data[index].title}'),
+                  Text('${data[index].created_at}')
+                ],
+              ),
+            ],
+          ),
         ),
-      );
+      ),
+    );
+  }
 }
